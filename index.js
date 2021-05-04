a74e5d58c9e595ce141ab3a9ad434030d31d0130
Abdallah Safar <abdallah.safar01@gmail.com>
2021-05-04 14:08:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function createEmployeeRecords(arr) {
 function createTimeInEvent(emp,date) {
   emp.timeInEvents.push({
     type: "TimeIn",
-    hour: date.split(' ')[1],
+    hour: parseInt(date.split(' ')[1]),
     date: date.split(' ')[0]
   });
   return emp;
