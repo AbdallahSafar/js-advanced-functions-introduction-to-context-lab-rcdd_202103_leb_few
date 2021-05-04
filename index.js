cc5522c0524366b9f08a21131c9fd26fbf1e621d
Abdallah Safar <abdallah.safar01@gmail.com>
2021-05-04 13:29:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+function createEmployeeRecord(arr) {
+  let employee = {};
+  employee['firstName'] = arr[0];
+  employee['familyName'] = arr[1];
+  employee['title'] = arr[2];
+  employee['payPerHour'] = arr[3];
+  employee['timeInEvents'] = [];
+  employee['timeOutEvents'] = [];
+  return employee;
+}
